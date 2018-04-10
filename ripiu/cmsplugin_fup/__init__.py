2d51d94e01d046342efdfd612f2919f115656f7a
geobaldi <geobaldi@gmail.com>
2018-04-10 13:11:00 +0200

    Version 0.2.0

--- ripiu/cmsplugin_fup/__init__.py
+++ ripiu/cmsplugin_fup/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.1.5'
+__version__ = '0.2.0'
 __author__ = 'matteo vezzola <matteo@studioripiu.it>'
 
 default_app_config = 'ripiu.cmsplugin_fup.apps.FUPConfig'
--- ripiu/cmsplugin_fup/static/ripiu/cmsplugin_fup/js/fup.js
+++ ripiu/cmsplugin_fup/static/ripiu/cmsplugin_fup/js/fup.js
@@ -1,14 +1,21 @@
 $(function() {
-    $(".fup > input").on("change", function() {
-        if ($(this).prop("checked")) {
-            $(".fup > input").not(this).prop("checked", false);
-        } 
+    $("mark.ripiu-fup > label").on("click", function(evt) {
+        var radio = $("#" + $(this).attr("for"));
+        radio.prop("checked", true);
+        radio.trigger("change");
+        return false;
     });
+
+    $(document).on("touchstart click", function(evt) {
+        $(".fup > input").prop("checked", false);
+    });
+
     $("mark.ripiu-fup > label").on("mouseover", function(e) {
         var radio = $("#" + $(this).attr("for"))
         radio.prop("checked", true);
         radio.trigger("change");
     });
+
     $("mark.ripiu-fup > label").on("mouseout", function(e) {
         var radio = $("#" + $(this).attr("for"))
         radio.prop("checked", false);
